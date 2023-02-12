b40af7b6282636e3361d82c0ab4f1548eaaca7ed
KiaPower666 <118475631+KiaPower666@users.noreply.github.com>
2023-02-12 23:12:53 +0300

    Add files via upload

---
+++ HomeWork5/script.js
@@ -0,0 +1,13 @@
+let uI = prompt("insert  url")
+function userinput()
+ {
+    
+     let picture = document.querySelector('.img')
+     let location = document.querySelector('#loc') 
+  picture.setAttribute('src', uI)
+ location.setAttribute('href', uI)
+ }
+
+ 
+ let h2 = document.querySelector('a').innerText = uI
+
